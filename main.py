1d1603f9743371ac00ad5c6b9cbf928e1b689e8c
changjiema2023@outlook.com <changjiema2023@gmail.com>
2023-10-29 19:45:53 -0700

    add simple first few functions

--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 import pyautogui
 import pyscreeze
 import random
+import time
 
 
 ABSOLUATE_PATH="D:\\automation\\wechat\\"
+BACK_IMAGE="pics\\Back.png"
+CHATS_IMAGE="pics\\Chats-white-english.png"
 DISCOVER_IMAGE="pics\\Discover-white-english.png"
+MOMENTS_IMAGE="pics\\Moments_icon.png"
+CAMERA_IMAGE="pics\\Camera.png"
 
 
 #location = pyautogui.center(res)
@@ -14,9 +19,28 @@ DISCOVER_IMAGE="pics\\Discover-white-english.png"
 
 #def generate_random_coordinates():
 
-def send_moments():
-    image_location = pyautogui.locateOnScreen(ABSOLUATE_PATH + DISCOVER_IMAGE, confidence=0.9)
+def main_page():
+    image_location = pyautogui.locateOnScreen(ABSOLUATE_PATH + BACK_IMAGE, confidence=0.8)
+    i = 0
+    while image_location is not None and i < 5:
+        left, top, width, height = image_location
 
+        # Calculate a random point within the located image
+        random_x = random.randint(left, left + width)
+        random_y = random.randint(top, top + height)
+
+        # Move the mouse to the random location
+        pyautogui.moveTo(random_x, random_y, duration=2)
+        pyautogui.click()
+        image_location = pyautogui.locateOnScreen(ABSOLUATE_PATH + BACK_IMAGE, confidence=0.8)
+        i = i + 1
+
+    image_location = pyautogui.locateOnScreen(ABSOLUATE_PATH + CHATS_IMAGE, confidence=0.8)
+    move_and_click(image_location=image_location)
+    print("We're at main page.")
+
+
+def move_and_click(image_location):
     # Check if the image was found
     if image_location is not None:
         # Extract the coordinates of the located image
@@ -34,9 +58,27 @@ def send_moments():
 
 
 
+def send_moments():
+    # Go to Discover.
+    image_location = pyautogui.locateOnScreen(ABSOLUATE_PATH + DISCOVER_IMAGE, confidence=0.8)
+    move_and_click(image_location=image_location)
+
+    # Go to Moments.
+    image_location = pyautogui.locateOnScreen(ABSOLUATE_PATH + MOMENTS_IMAGE, confidence=0.8)
+    move_and_click(image_location=image_location)
+
+    time.sleep(2)
+
+    # Go to Camera.
+    image_location = pyautogui.locateOnScreen(ABSOLUATE_PATH + CAMERA_IMAGE, confidence=0.8)
+    move_and_click(image_location=image_location)
+
+
 
 if __name__ == "__main__":
-    
+    # Go to main page.
+    main_page()
+
     # send friend
     send_moments()
  
